3503147100045718d38cbe4d673c44c964d566ac
jeffersoncaarvalho12-design <jeffersoncaarvalho12@gmail.com>
2026-04-02 23:52:37 -0300

    Update MainActivity.kt

--- app/src/main/java/com/technet/olttv/MainActivity.kt
+++ app/src/main/java/com/technet/olttv/MainActivity.kt
@@ -3,9 +3,12 @@ package com.technet.olttv
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.technet.olttv.ui.AuthViewModel
 import com.technet.olttv.ui.DashboardTvScreen
 import com.technet.olttv.ui.DashboardViewModel
+import com.technet.olttv.ui.PinLoginScreen
 import com.technet.olttv.ui.theme.OLTTVMonitorTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,8 +17,17 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             OLTTVMonitorTheme {
-                val vm: DashboardViewModel = viewModel()
-                DashboardTvScreen(viewModel = vm)
+                val authViewModel: AuthViewModel = viewModel()
+                val dashboardViewModel: DashboardViewModel = viewModel()
+
+                if (authViewModel.isAuthenticated) {
+                    DashboardTvScreen(
+                        viewModel = dashboardViewModel,
+                        onLogout = { authViewModel.logout() }
+                    )
+                } else {
+                    PinLoginScreen(viewModel = authViewModel)
+                }
             }
         }
     }
